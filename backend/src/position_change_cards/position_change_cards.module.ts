6eb447b5a77fbf9f01bc6986dcaf59efd71f0a5a
xaviza11 <xaviza11@gmail.com>
2025-12-08 19:12:25 +0100

    add changes for start app

--- backend/src/position_change_cards/position_change_cards.module.ts
+++ backend/src/position_change_cards/position_change_cards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PositionChangeCardsController } from './position_change_cards.controller';
 import { PositionChangeCardsService } from './position_change_cards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Team } from '../entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Team])],
   controllers: [PositionChangeCardsController],
-  providers: [PositionChangeCardsService]
+  providers: [PositionChangeCardsService],
 })
 export class PositionChangeCardsModule {}
