91d6b7ccfa3047f08c5d30f2ac97df49784281d3
xaviza11 <xaviza11@gmail.com>
2025-12-08 17:11:25 +0100

    refactor position_change_card

--- backend/src/entities/position_change_cards.entity.ts
+++ backend/src/entities/position_change_cards.entity.ts
@@ -1,11 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, JoinColumn } from 'typeorm';
 import { Storage } from './';
+import { Positions } from '../enums';
 
 @Entity()
 export class PositionChangeCard {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ManyToOne(() => Storage, storage => storage.position_change_cards, { onDelete: 'CASCADE' })
+  @Column({ type: 'enum', enum: Positions })
+  new_position: Positions;
+
+  @Column({ type: 'uuid' })
+  storage_id: string;
+
+  @ManyToOne(() => Storage, storage => storage.position_change_cards, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'storage_id' })
   storage: Storage;
 }
